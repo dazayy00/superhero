811e400c3f94487a6b7b07fb0aaf82fa78eab3b8
dazayy <zs21020562@estudiantes.uv.mx>
2024-03-19 09:11:35 -0600

    update db

--- controllers/index.js
+++ controllers/index.js
@@ -2,20 +2,21 @@ const models = require("../database/models");
 
 const createUser = async (req, res) => {
 	try{
-		const user = await models.superhero.create(req.body);
-		return res.status(201).json({
+		const user = await models.SUPERHERO.create(req.body);
+		return res.status(200).json({
 			user
 		});
 	} catch (error) {
-		return res.status(500).json({ error : error.message});
+		return res.status(500).json({ error : error.message });
 	}
 };
 
 const getAllUsers = async (req, res) => {
 	console.log('getting users');
 	try { 
-		const users = await models.superhero.findAll({
-			include: []
+		const users = await models.SUPERHERO.findAll({
+			include: [
+				]
 		});
 		return res.status(200).json({ users });
 	} catch (error) {
